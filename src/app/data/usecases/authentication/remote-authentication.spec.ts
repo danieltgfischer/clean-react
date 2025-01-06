94657d28004f09a70673f8e32f45ef828df45a0e
Daniel Fischer <danieltgfischer@gmail.com>
2025-01-06 16:43:09 -0300

    test: add UnexpectedError handling for various HttpPostClient error responses in RemoteAuthentication

--- src/app/data/usecases/authentication/remote-authentication.spec.ts
+++ src/app/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,9 +1,9 @@
 import { faker } from "@faker-js/faker";
 import { HttpPostClientSpy } from "@data/test";
+import { HttpStatusCode } from "@data/protocols/http";
 import { mockAuthentication } from "@domain/test";
-import { InvalidCredentialsError } from "@domain/errors";
+import { InvalidCredentialsError, UnexpectedError } from "@domain/errors";
 import { RemoteAuthentication } from "./remote-authentication";
-import { HttpStatusCode } from "../../protocols/http";
 
 type SutTypes = {
   sut: RemoteAuthentication;
@@ -47,5 +47,41 @@ describe("RemoteAuthentication", () => {
     const promise = sut.auth(mockAuthentication());
 
     await expect(promise).rejects.toThrow(new InvalidCredentialsError());
+  });
+
+  test("Should throw UnexpectedError if HttpPostClient return 400", async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest,
+    };
+
+    const promise = sut.auth(mockAuthentication());
+
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+
+  test("Should throw UnexpectedError if HttpPostClient return 404", async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.notFound,
+    };
+
+    const promise = sut.auth(mockAuthentication());
+
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+
+  test("Should throw UnexpectedError if HttpPostClient return 500", async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.serverError,
+    };
+
+    const promise = sut.auth(mockAuthentication());
+
+    await expect(promise).rejects.toThrow(new UnexpectedError());
   });
 });
