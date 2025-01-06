952db5f5cd9977a307cac965f07cb3c269d6ef87
Daniel Fischer <danieltgfischer@gmail.com>
2025-01-06 11:12:50 -0300

    test: ensure HttpPostClient calls with correct body

--- src/app/data/usecases/authentication/remote-authentication.spec.ts
+++ src/app/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,5 +1,6 @@
 import { faker } from "@faker-js/faker";
 import { HttpPostClientSpy } from "@data/test";
+import { mockAuthentication } from "@domain/test";
 import { RemoteAuthentication } from "./remote-authentication";
 
 type SutTypes = {
@@ -21,7 +22,16 @@ describe("RemoteAuthentication", () => {
     const url = faker.internet.url();
     const { sut, httpPostClientSpy } = makeSut(url);
 
-    await sut.auth();
+    await sut.auth(mockAuthentication());
     expect(httpPostClientSpy.url).toBe(url);
+  });
+
+  test("Should call HttpPostClient with correct body", async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+
+    const authenticationParams = mockAuthentication();
+
+    await sut.auth(authenticationParams);
+    expect(httpPostClientSpy.body).toBe(authenticationParams);
   });
 });
